9ad7ed889fb40361de5e42590132395f6aff1c80
Matteo Carnelos <matteo.carnelos@studenti.unipd.it>
2021-08-31 18:40:33 +0200

    Add buggy baseline

---
+++ kademlia.go
@@ -0,0 +1,16 @@
+package kademlia_go
+
+type Kademlia struct {
+}
+
+func (kademlia *Kademlia) LookupContact(target *Contact) {
+	// TODO
+}
+
+func (kademlia *Kademlia) LookupData(hash string) {
+	// TODO
+}
+
+func (kademlia *Kademlia) Store(data []byte) {
+	// TODO
+}
