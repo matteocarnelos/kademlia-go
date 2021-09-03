55dde155d94b27c087d011fc7e5b14a296c47cff
Matteo Carnelos <matteo.carnelos@studenti.unipd.it>
2021-09-03 16:09:52 +0200

    Add base cli interface

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Kademlia node started!")
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print(">>> ")
+	for scanner.Scan() {
+		switch scanner.Text() {
+		case "put":
+			fmt.Println("put command received")
+		case "get":
+			fmt.Println("get command received")
+		case "exit":
+			os.Exit(0)
+		case "":
+		default:
+			fmt.Println("usage: [put/get/exit] ...")
+		}
+		fmt.Print(">>> ")
+	}
 }
