3141b256dbc6da4a2827e6e1b606e4030234b169
Matteo Carnelos <matteo.carnelos@studenti.unipd.it>
2021-10-14 17:40:56 +0200

    Add RESTful interface

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 	"encoding/hex"
 	"fmt"
 	"github.com/matteocarnelos/kadlab/kademlia"
+	"io/ioutil"
 	"math/rand"
 	"net"
+	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -21,6 +23,63 @@ const ListenIP = "0.0.0.0"
 const ListenDelaySec = 5
 
 const CLIPrefix = ">>>"
+
+var kdm *kademlia.Kademlia
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	ip := strings.Split(r.RemoteAddr, ":")[0]
+	body, _ := ioutil.ReadAll(r.Body)
+	fmt.Printf("\n%s -> [%s %s %s] %s\n", ip, r.Method, r.URL, r.Proto, body)
+	var msg string
+	var code int
+	switch r.Method {
+	case "GET":
+		hash := strings.Split(r.URL.String(), "/")[2]
+		if len(hash) != 40 {
+			code = http.StatusBadRequest
+			msg = "Invalid hash, please provide a valid 160-bit data hash"
+			break
+		}
+		if content, ok := load(hash); ok {
+			code = http.StatusOK
+			msg = content
+		} else {
+			code = http.StatusNotFound
+			msg = "Object not found"
+		}
+	case "POST":
+		if len(body) > 255 {
+			code = http.StatusBadRequest
+			msg = "Invalid object size, maximum size is 255 bytes"
+			break
+		}
+		hash := store(string(body))
+		w.Header().Set("Location", "/objects/" + hash)
+		code = http.StatusCreated
+		msg = "Object stored!"
+	}
+	w.WriteHeader(code)
+	fmt.Fprintln(w, msg)
+	fmt.Printf("[%s %d %s] %s -> %s\n\n", r.Proto, code, http.StatusText(code), msg, ip)
+}
+
+func store(content string) string {
+	fmt.Println("Storing object...")
+	hash := kdm.Store([]byte(content))
+	fmt.Println("Object stored!")
+	fmt.Println()
+	return hash
+}
+
+func load(hash string) (string, bool) {
+	fmt.Println("Finding object...")
+	if data, ok := kdm.LookupData(hash); ok {
+		fmt.Println("Object found!")
+		fmt.Println()
+		return data.(string), true
+	}
+	return "", false
+}
 
 func main() {
 	iface, _ := net.InterfaceByName("eth0")
@@ -40,7 +99,7 @@ func main() {
 	fmt.Println()
 
 	me := kademlia.NewContact(id, ip.String())
-	kdm := kademlia.NewKademlia(me)
+	kdm = kademlia.NewKademlia(me)
 	kdm.StartListen(ListenIP, ListenPort)
 	delay := time.Duration(ListenDelaySec + rand.Intn(5))
 	time.Sleep(delay * time.Second)
@@ -56,6 +115,10 @@ func main() {
 		fmt.Println("Network joined!")
 		fmt.Println()
 	}
+
+	http.HandleFunc("/objects", handleRequest)
+	http.HandleFunc("/objects/", handleRequest)
+	go http.ListenAndServe(":80", nil)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
@@ -77,10 +140,7 @@ func main() {
 				fmt.Println("Invalid object size, maximum size is 255 bytes")
 				break
 			}
-			fmt.Println("Storing object...")
-			hash := kdm.Store([]byte(args[0]))
-			fmt.Println("Object stored!")
-			fmt.Println()
+			hash := store(args[0])
 			fmt.Printf("Object hash: %s\n\n", hash)
 		case "get":
 			if len(args) != 1 {
@@ -89,16 +149,13 @@ func main() {
 				break
 			}
 			if len(args[0]) != 40 {
-				fmt.Println("Invalid hash, please provide a 160-bit data hash")
+				fmt.Println("Invalid hash, please provide a valid 160-bit data hash")
 				break
 			}
-			fmt.Println("Finding object...")
-			if data, ok := kdm.LookupData(args[0]); ok {
-				fmt.Println("Object found!")
-				fmt.Println()
-				fmt.Printf("Object content: %s\n\n", data)
+			if content, ok := load(args[0]); ok {
+				fmt.Printf("Object content: %s\n\n", content)
 			} else {
-				fmt.Println("Object not found")
+				fmt.Printf("Object not found\n\n")
 			}
 		case "":
 		case "exit":
